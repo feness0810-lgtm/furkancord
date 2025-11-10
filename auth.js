0ef290070329648f16b0541b0c15a547bd1e61a6
feness0810-lgtm <feness0810@gmail.com>
2025-11-10 15:12:18 +0300

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,15 @@
+function registerUser(event) {
+  event.preventDefault();
+
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  const user = {
+    username: username,
+    email: email,
+    password: password
+  };
+
+  localStorage.setItem("user", JSON.stringify(user));
+  alert("Kayıt başarılı! Giriş sayfasına yönlendir
